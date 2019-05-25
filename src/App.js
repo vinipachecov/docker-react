98e6414357b6f0ac544a74082eb53d891f0ea23b
Vinícius Vieira <vinipachecov@gmail.com>
2019-05-25 11:36:18 -0300

    change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DOCKER ELASTICBEANSTALK FROM FEATURE
+          DOCKER ELASTICBEANSTALK
         </p>
         <a
           className="App-link"
